e325f6846bb46da03d7271f6df16300f570d0eea
Noyon-Tech-Solution <shahadathosen8987@gmail.com>
2024-11-28 21:30:41 +0600

    Noyon Tech Solution

--- TypeScript_Tutorial/script.ts
+++ TypeScript_Tutorial/script.ts
@@ -28,3 +28,55 @@
 // }
 // myFun("10", "40");
 // myFun("20", "50");
+
+
+
+///!delet kora diva kalka
+
+let myFunction = (num1:number, num2:number) => {
+    console.log(num1 + num2)
+}
+myFunction(10, 20);
+myFunction(10, 20);
+
+let myFun = (num1:string, num2:string) => {
+    console.log(num1 + num2)
+}
+myFun("10", "40");
+myFun("20", "50");
+
+let myFunction1 = (num1:number, num2:number) => {
+    console.log(num1 + num2)
+}
+myFunction1(10, 20);
+myFunction1(10, 20);
+
+let myFun1 = (num1:string, num2:string) => {
+    console.log(num1 + num2)
+}
+myFun1("10", "40");
+myFun1("20", "50");
+
+let myFunction2 = (num1:number, num2:number) => {
+    console.log(num1 + num2)
+}
+myFunction2(10, 20);
+myFunction2(10, 20);
+
+let myFun2 = (num1:string, num2:string) => {
+    console.log(num1 + num2)
+}
+myFun2("10", "40");
+myFun2("20", "50");
+
+let myFun3 = (num1:number, num2:number) => {
+    console.log(num1 + num2)
+}
+myFun3(10, 20);
+myFun3(10, 20);
+
+let myFun4 = (num1:string, num2:string) => {
+    console.log(num1 + num2)
+}
+myFun4("10", "40");
+myFun4("20", "50");
